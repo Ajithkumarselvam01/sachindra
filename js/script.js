1f2aaa99b3ae8aa350a2a8735f1a7eb0721c38de
AJITHKUMAR S <ajithkumar.s@grocat.in>
2024-10-29 13:59:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
       responsive: {
           0: { items: 2 },
           600: { items: 2 },
-          1000: { items: 4 }
+          1000: { items: 5 }
       }
   });
 });
